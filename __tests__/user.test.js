f2a2f1be7395bd73c65974791f602bac8f46bff8
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-06-09 13:51:44 -0700

    made basic structure of user test

---
+++ __tests__/user.test.js
@@ -0,0 +1,32 @@
+const { MongoMemoryServer } = require('mongodb-memory-server'); 
+const mongod = new MongoMemoryServer(); 
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect.js'); 
+
+// const request = require('supertest'); 
+// const app = require('../lib/app'); 
+// const User = require('../lib/models/User.js'); 
+
+describe('user routes', () => {
+
+  beforeAll(async() => {
+    const uri = await mongod.getUri();
+    return connect(uri);
+  });
+    
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+    
+  afterAll(async() => {
+    await mongoose.connection.close();
+    return mongod.stop();
+  });
+
+  //   it('creates an organization via POST', () => {
+    
+
+  //   });
+
+
+}); 
