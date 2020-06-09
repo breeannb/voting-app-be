8afdf9b988d92ae8fce23c5753373a9578c0f7e1
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-06-09 14:31:14 -0700

    was able to write first route, not yet passing test though but did delete an extra file I did not need

--- __tests__/organization.test.js
+++ __tests__/organization.test.js
@@ -23,6 +23,7 @@ describe('organization routes', () => {
     return mongod.stop();
   });
 
+  //   the create route will be used when a new organization is created
   it('creates an organization via POST', () => {
     return request(app)
       .post('/api/v1/votes/organization')
@@ -43,5 +44,13 @@ describe('organization routes', () => {
 
   });
 
+
+  // the get all route will be used to see all organizations (_id, title, and imageUrl of organization only)
+
+  // the get by id route will be used to get details about an organization
+
+  // the update route will be used to update organization information
+  
+  // the delete route will be used if an organization is disbanded
 
 }); 
