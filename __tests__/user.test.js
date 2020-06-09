57fb257f8bed60973c40477636c4a96eaba378fb
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-06-09 16:49:54 -0700

    created a test for user to get by id via Get route

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -3,9 +3,9 @@ const mongod = new MongoMemoryServer();
 const mongoose = require('mongoose');
 const connect = require('../lib/utils/connect.js'); 
 
-// const request = require('supertest'); 
-// const app = require('../lib/app'); 
-// const User = require('../lib/models/User.js'); 
+const request = require('supertest'); 
+const app = require('../lib/app'); 
+const User = require('../lib/models/User.js'); 
 
 describe('user routes', () => {
 
@@ -23,10 +23,25 @@ describe('user routes', () => {
     return mongod.stop();
   });
 
-  //   it('creates an organization via POST', () => {
-    
-
-  //   });
+  it('gets an user by id via GET', () => {
+    return User.create({
+      name: 'Breeann B',
+      phone: '(570)404-5230', 
+      email: 'bolinskybm10@gmail.com',
+      communicationMedium: 'email'
+    })
+      .then(user => request(app).get(`api/v1/users/${user._id}`))
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          name: 'Breeann B',
+          phone: '(570)404-5230', 
+          email: 'bolinskybm10@gmail.com',
+          communicationMedium: 'email',
+          __v: 0
+        });
+      });
+  });
 
 
 }); 
