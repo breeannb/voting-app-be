235c7b6169f0ab3e80564174031ac58fcb93007f
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-06-11 16:20:20 -0700

    got some help with not populating organization for poll, got test to pass and post route made with silver room help

--- __tests__/vote.test.js
+++ __tests__/vote.test.js
@@ -8,11 +8,91 @@ const connect = require('../lib/utils/connect.js');
 
 const request = require('supertest'); 
 const app = require('../lib/app'); 
-const Vote = require('../lib/models/Vote.js'); 
+// const Vote = require('../lib/models/Vote.js');
+const User = require('../lib/models/User');
+const Poll = require('../lib/models/Poll'); 
+const Organization = require('../lib/models/Organization'); 
 
-describe('user routes', () => {
+beforeAll(async() => {
+  const uri = await mongod.getUri();
+  return connect(uri);
+});
+  
+beforeEach(() => {
+  return mongoose.connection.dropDatabase();
+});
+
+//create an organization
+let organization;
+beforeEach(async() => {
+  organization = await Organization.create({
+    title: 'League of Women Voters Organization',
+    description: 'League of Women Group for Voting Info',
+    imageUrl: 'image2.com'
+  });
+});
+
+//create one poll
+let poll;
+beforeEach(async() => {
+  poll = await Poll.create({
+    organization: organization.id,
+    title: 'A Poll to Save Older Forests, the Owl\'s Habitat',
+    description: 'A vote for the Owls, please give a Hoot', 
+    options: 'Yes'
+  });
+});
+  
+//create one user
+let userOne;
+beforeEach(async() => {
+  userOne = await User.create({
+    name: 'Sally', 
+    phone: '(570)404-5231', 
+    email: 'sally@sally.com', 
+    imageUrl: 'image11.com',
+    communicationMedium: 'email'
+  });
+});
+
+// //create second user
+// let userTwo;
+// beforeEach(async() => {
+//   userTwo = await User.create({
+//     name: 'Sam', 
+//     phone: '(570)404-5232', 
+//     email: 'sam@sam.com', 
+//     imageUrl: 'image12.com',
+//     communicationMedium: 'email'
+//   });
+// });
+  
+afterAll(async() => {
+  await mongoose.connection.close();
+  return mongod.stop();
+});
+
+
+describe('vote routes', () => {
 
   it('creates a new vote using POST', () => {
+
+    return request(app)
+      .post('/api/v1/votes')
+      .send({
+        poll: poll.id,
+        user: userOne.id,
+        options: 'Yes',
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          poll: poll.id,
+          user: userOne.id, 
+          options: 'Yes',
+          __v: 0
+        });
+      });
 
   });
 });
