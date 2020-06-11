9858a68658aadd573ca39630eb4a0c9c7a7e098f
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-06-11 11:38:42 -0700

    made a shell of files for membership, created model and added virtuals, now going through tests for organization tests

--- __tests__/organization.test.js
+++ __tests__/organization.test.js
@@ -43,8 +43,7 @@ describe('organization routes', () => {
           _id: expect.anything(),
           title: 'Environmental Voter Project 1', 
           description: 'description1', 
-          imageUrl: 'image1.com', 
-          __v: 0
+          imageUrl: 'image1.com'
         });
       });
   });
