c2649eb3b81c95072ec2e2f3f73433e0d68187a3
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-06-11 12:05:46 -0700

    created test for creating a membership with POST

--- __tests__/membership.test.js
+++ __tests__/membership.test.js
@@ -5,9 +5,9 @@ const connect = require('../lib/utils/connect.js');
 
 const request = require('supertest'); 
 const app = require('../lib/app'); 
-const Organization = require('../lib/models/Organization'); 
+const Membership = require('../lib/models/Membership'); 
 
-describe('organization routes', () => {
+describe('membership routes', () => {
 
   beforeAll(async() => {
     const uri = await mongod.getUri();
@@ -17,13 +17,6 @@ describe('organization routes', () => {
   beforeEach(() => {
     return mongoose.connection.dropDatabase();
   });
-  
-  // let poll; 
-  // beforeEach(async() => {
-  //   poll = await poll.create({
-  //     create
-  //   });
-  // }); 
 
   afterAll(async() => {
     await mongoose.connection.close();
@@ -31,23 +24,22 @@ describe('organization routes', () => {
   });
 
   // the create route will be used to create a new membership
-  it('creates a membership via POST', () => {
+  it('create a membership via POST', () => {
     return request(app)
-      .post('/api/v1/organizations')
+      .post('/api/v1/membership')
       .send({
-        title: 'Environmental Voter Project 1',
-        description: 'description1',
-        imageUrl: 'image1.com'
+        organization: 'League of Women Voters Organization',
+        user: 'Sally'
       })
-      .then (res => {
+      .then(res => {
         expect(res.body).toEqual({
           _id: expect.anything(),
-          title: 'Environmental Voter Project 1', 
-          description: 'description1', 
-          imageUrl: 'image1.com'
+          organization: 'League of Women Voters Organization',
+          user: 'Sally', 
+          __v: 0
         });
       });
-  }); 
+  });
 
   // (/api/v1/memberships?org=ORG_ID) the get all route will be used to see all users in an organization (_id, name, and imageUrl only)
 
