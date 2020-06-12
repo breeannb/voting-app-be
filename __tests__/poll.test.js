3b20ac29023fb7349375103ae135112de722382f
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-06-12 12:51:02 -0700

    created test for delete poll and deletes all associated votes

--- __tests__/poll.test.js
+++ __tests__/poll.test.js
@@ -7,6 +7,8 @@ const request = require('supertest');
 const app = require('../lib/app'); 
 const Poll = require('../lib/models/Poll'); 
 const Organization = require('../lib/models/Organization'); 
+const User = require('../lib/models/User');
+const Vote = require('../lib/models/Vote');
 
 describe('poll routes', () => {
 
@@ -25,6 +27,28 @@ describe('poll routes', () => {
       title: 'Environmental Organization for Voters',
       description: 'A Voting Party for Environmental Factors',
       imageUrl: 'image1.com'
+    });
+  });
+
+  let userOne;
+  beforeEach(async() => {
+    userOne = await User.create({
+      name: 'Sally', 
+      phone: '(570)404-5231', 
+      email: 'sally@sally.com', 
+      imageUrl: 'image11.com',
+      communicationMedium: 'email'
+    });
+  });
+
+  let userTwo;
+  beforeEach(async() => {
+    userTwo = await User.create({
+      name: 'Sam', 
+      phone: '(570)404-5232', 
+      email: 'sam@sam.com', 
+      imageUrl: 'image12.com',
+      communicationMedium: 'email'
     });
   });
 
@@ -123,27 +147,74 @@ describe('poll routes', () => {
       });
   });
  
-  it('deletes a polls by id via DELETE', () => {
-    return Poll.create({
-      organization: organization._id,
-      title: 'A Poll to Save Older Forests, the Owl\'s Habitat',
-      description: 'A vote for the Owls, please give a Hoot', 
-      options: 'Yes', 
-      __v: 0
-    })
-      .then(poll => request(app).delete(`/api/v1/polls/${poll._id}`))
-      .then(res => {
-        expect(res.body).toEqual({
-          _id: expect.anything(),
-          organization: organization.id,
-          title: 'A Poll to Save Older Forests, the Owl\'s Habitat',
-          description: 'A vote for the Owls, please give a Hoot', 
-          options: 'Yes', 
-          __v: 0
-        });
-      });
-  });
+  // it('deletes a polls by id via DELETE', () => {
+  //   return Poll.create({
+  //     organization: organization._id,
+  //     title: 'A Poll to Save Older Forests, the Owl\'s Habitat',
+  //     description: 'A vote for the Owls, please give a Hoot', 
+  //     options: 'Yes', 
+  //     __v: 0
+  //   })
+  //     .then(poll => request(app).delete(`/api/v1/polls/${poll._id}`))
+  //     .then(res => {
+  //       expect(res.body).toEqual({
+  //         _id: expect.anything(),
+  //         organization: organization.id,
+  //         title: 'A Poll to Save Older Forests, the Owl\'s Habitat',
+  //         description: 'A vote for the Owls, please give a Hoot', 
+  //         options: 'Yes', 
+  //         __v: 0
+  //       });
+  //     });
+  // });
 
   //deletes a poll and then deletes votes associated with poll 
 
+  it('deletes a poll and deletes votes associated with poll', async() => {
+    const organization = await Organization.create({
+      title: 'Environmental Voter Project 1', 
+      description: 'description1', 
+      imageUrl: 'image1.com'
+    });
+
+    const poll = await Poll.create({
+      organization: organization._id,
+      title: 'Poll 1 Title',
+      description: 'Poll 1 Description', 
+      options: 'Yes', 
+      __v: 0
+    });
+
+    await Vote.create([
+      {
+        poll: poll.id,
+        user: userOne.id,
+        options: 'Yes',
+      },
+      {
+        poll: poll.id,
+        user: userTwo.id,
+        options: 'Yes',
+      }
+    ]);
+
+    return request(app)
+      .delete(`/api/v1/polls/${poll._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          organization: organization._id,
+          title: 'Poll 1 Title',
+          description: 'Poll 1 Description', 
+          options: 'Yes', 
+          __v: 0
+        });
+
+        return Vote.find({ poll: poll._id });
+      })
+      .then(votes => {
+        expect(votes).toEqual([]);
+      });
+  });
+
 });
+
