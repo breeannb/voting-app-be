06f679742f6aaf5a0908590af4c88bcbf47f07e7
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-06-16 14:38:43 -0700

    created signup route with help from mercury peeps

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -1,12 +1,41 @@
-const { Router } = require('express');
+require('dotenv').config();
+
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongod = new MongoMemoryServer();
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect');
+
+const request = require('supertest');
+const app = require('../lib/app');
 const User = require('../lib/models/User');
-const ensureAuth = require('../lib/middleware/ensureAuth');
 
-const setCookie = (user, res) => {
-  res.cookie('session', user.authToken(), {
-    maxAge: 1000 * 60 * 60 * 24,
-    httpOnly: true
+describe('auth routes', () => {
+  beforeAll(async() => {
+    const uri = await mongod.getUri();
+    return connect(uri);
   });
-};
 
-module.exports = Router();
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(async() => {
+    await mongoose.connection.close();
+    return mongod.stop();
+  });
+
+  it('can signup a new user via POST', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'breeanntest@breeanntest.com',
+        password: 'password1234'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          email: 'breeanntest@breeanntest.com'
+        });
+      });
+  });
+});
